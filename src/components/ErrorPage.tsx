c126910ad56c75a08bf590bf72bf392e7c3ea240
royisy <28505196+royisy@users.noreply.github.com>
2022-12-29 21:37:09 +0900

    fix tags

--- src/components/ErrorPage.tsx
+++ src/components/ErrorPage.tsx
@@ -1,7 +1,10 @@
+import { Link } from "react-router-dom";
+
 export default function ErrorPage() {
     return (
-        <div>
+        <>
             <h1>Error</h1>
-        </div>
+            <p><Link to="/">Home</Link></p>
+        </>
     );
 }
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,9 +2,9 @@ import { Link } from "react-router-dom";
 
 export default function Home() {
     return (
-        <div>
+        <>
             <h1>Trilingo</h1>
-            <Link to="menu">Menu</Link>
-        </div >
+            <p><Link to="menu">Menu</Link></p>
+        </>
     );
 }
