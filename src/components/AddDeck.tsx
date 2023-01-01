71350fdec80bf1c7893664e627575edf948f9fd6
royisy <28505196+royisy@users.noreply.github.com>
2023-01-01 13:52:04 +0900

    add deck-list

--- src/components/AddDeck.tsx
+++ src/components/AddDeck.tsx
@@ -1,10 +1,27 @@
+import { parse } from "papaparse";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function AddDeck() {
+    const [deckList, setDeckList] = useState<{ id: number, title: string }[]>([]);
+    useEffect(() => {
+        fetch("/deck-list.csv")
+            .then(res => res.text())
+            .then(csv => {
+                parse(csv, {
+                    header: true,
+                    complete: (result: any) => setDeckList(result.data),
+                    skipEmptyLines: true
+                });
+            });
+    }, []);
     return (
         <>
             <h1>Add deck</h1>
             <p><Link to="/menu">Menu</Link></p>
+            <ul>
+                {deckList.map(deck => <li>{deck.title}</li>)}
+            </ul>
         </ >
     );
 }
