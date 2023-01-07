767c995de4e22327938b3713de70b8cd08c34943
royisy <28505196+royisy@users.noreply.github.com>
2023-01-07 16:38:09 +0900

    add deck list to menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,12 +1,32 @@
+import { useLiveQuery } from "dexie-react-hooks";
 import { Link } from "react-router-dom";
+import { db } from "../db";
+import { IDeck } from "../models/Deck";
 
 export default function Menu() {
+    const decks = useLiveQuery(
+        () => db.decks.toArray()
+    );
+
     return (
         <>
             <h1>Menu</h1>
             <p><Link to="/">Home</Link></p>
+            <ul>
+                {decks?.map(deck => <DeckItem key={deck.id} deck={deck} />)}
+            </ul>
             <p><Link to="add-deck">Add deck</Link></p>
             <p><Link to="delete-deck">Delete deck</Link></p>
         </>
+    );
+}
+
+function DeckItem({ deck }: { deck: IDeck }) {
+    async function selectDeck() {
+        console.log(deck.id);
+    }
+
+    return (
+        <li onClick={selectDeck}>{deck.title}</li>
     );
 }
