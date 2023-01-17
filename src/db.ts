9bb9fdea9500043af9f3f8bb895fccebeafec6f9
royisy <28505196+royisy@users.noreply.github.com>
2023-01-17 16:20:58 +0900

    remove type

--- src/db.ts
+++ src/db.ts
@@ -1,7 +1,7 @@
 import Dexie, { Table } from "dexie";
-import type { AppSetting } from "./models/AppSetting";
-import type { Deck } from "./models/Deck";
-import type { Word } from "./models/Word";
+import { AppSetting } from "./models/AppSetting";
+import { Deck } from "./models/Deck";
+import { Word } from "./models/Word";
 
 export class TrilingoDatabase extends Dexie {
     appSettings!: Table<AppSetting>;
