b2c681f1f63c8ada46985cc6979c53383a6f3be2
royisy <28505196+royisy@users.noreply.github.com>
2023-01-22 16:30:12 +0900

    implement getWords in Practice

--- src/components/Practice.tsx
+++ src/components/Practice.tsx
@@ -1,13 +1,16 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { Deck } from "../models/Deck";
+import { Word } from "../models/Word";
 import { AppSettingRepository } from "../repositories/AppSettingRepository";
 import { DeckRepository } from "../repositories/DeckRepository";
+import { WordRepository } from "../repositories/WordRepository";
 
 export function Practice() {
-    const [title, setTitle] = useState<string>();
+    const NUM_OF_WORDS = 10;
+    const [words, setWords] = useState<Word[]>([]);
+    const [index, setIndex] = useState<number>(0);
 
-    async function getDeck() {
+    async function getWords() {
         const appSettingRepo = new AppSettingRepository();
         const appSetting = await appSettingRepo.get();
         const deckId = appSetting.selectedDeckId;
@@ -16,17 +19,35 @@ export function Practice() {
         }
         const deckRepo = new DeckRepository();
         const deck = await deckRepo.getById(deckId);
-        setTitle(deck?.title);
+        if (!deck) {
+            throw new Error("Deck not found.");
+        }
+        const wordRepo = new WordRepository();
+        let words = await wordRepo.getByIncorrectCnt(deckId, NUM_OF_WORDS);
+        if (words.length < NUM_OF_WORDS) {
+            const wordsByCorrectCnt = await wordRepo.getByCorrectCnt(deckId, NUM_OF_WORDS - words.length);
+            words = words.concat(wordsByCorrectCnt);
+        }
+        if (words.length === 0) {
+            throw new Error("No words.");
+        }
+        words = words.sort(() => Math.random() - 0.5);
+        setWords(words);
     }
 
     useEffect(() => {
-        getDeck();
+        if (words.length === 0) {
+            getWords();
+        }
     }, []);
 
     return (
         <>
-            <h1>{title}</h1>
             <p><Link to="/">Quit</Link></p>
+            <p>{index + 1} / {NUM_OF_WORDS}</p>
+            <p>{words[index]?.definition}</p>
+            <p><input type="text" /></p>
+            <p><button>Skip</button></p>
         </>
     );
 }
