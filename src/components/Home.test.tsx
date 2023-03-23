9711afabe0f1274f2186c02c38fe4fbf870e4e68
royisy <28505196+royisy@users.noreply.github.com>
2023-03-23 22:02:58 +0900

    use database instead of mock

--- src/components/Home.test.tsx
+++ src/components/Home.test.tsx
@@ -1,37 +1,26 @@
 import { render, screen, waitFor } from '@testing-library/react'
 import { MemoryRouter } from 'react-router-dom'
-import { vi } from 'vitest'
+import { db } from '../db'
 import { AppSetting } from '../models/AppSetting'
 import { Deck } from '../models/Deck'
 import { Word } from '../models/Word'
 import { Home } from './Home'
 
-vi.mock('../repositories/appSetting', () => ({
-  getAppSetting: () => {
+describe('Home', () => {
+  it('should render deck title and words list', async () => {
     const appSetting = new AppSetting()
     appSetting.selectedDeckId = 1
-    return appSetting
-  },
-}))
-
-vi.mock('../repositories/deck', () => ({
-  getDeckById: () => new Deck(1, 'Deck title'),
-}))
-
-vi.mock('../repositories/word', () => ({
-  getWordsByDeckId: () => {
+    await db.appSettings.add(appSetting)
+    const deck = new Deck(1, 'deck 1')
+    await db.decks.add(deck)
     const word1 = new Word(1, 1, 'definition 1', 'answer 1')
     word1.correctCnt = 0
     word1.skippedCnt = 0
+    await db.words.add(word1)
     const word2 = new Word(1, 2, 'definition 2', 'answer 2')
     word2.correctCnt = 0
     word2.skippedCnt = 0
-    return [word1, word2]
-  },
-}))
-
-describe('Home', () => {
-  it('should render deck title and words list', async () => {
+    await db.words.add(word2)
     render(
       <MemoryRouter>
         <Home />
@@ -45,7 +34,7 @@ describe('Home', () => {
     expect(screen.queryByText('definition 2')).not.toBeInTheDocument()
     expect(screen.queryByText('answer 2')).not.toBeInTheDocument()
     await waitFor(() => {
-      expect(screen.getByText('Deck title')).toBeInTheDocument()
+      expect(screen.getByText('deck 1')).toBeInTheDocument()
       expect(screen.getByText('Start')).toBeInTheDocument()
       expect(screen.getByRole('table')).toBeInTheDocument()
       expect(screen.getByText('definition 1')).toBeInTheDocument()
@@ -53,5 +42,11 @@ describe('Home', () => {
       expect(screen.getByText('definition 2')).toBeInTheDocument()
       expect(screen.queryByText('answer 2')).toBeInTheDocument()
     })
+  })
+
+  afterAll(async () => {
+    await db.appSettings.clear()
+    await db.decks.clear()
+    await db.words.clear()
   })
 })
