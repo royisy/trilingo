fab4025d54ab94266b9e9079e4799402b1eae6b8
royisy <28505196+royisy@users.noreply.github.com>
2023-04-06 13:09:16 +0900

    add eslint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -20,9 +20,13 @@ module.exports = {
   ignorePatterns: ['vite.config.ts'],
   rules: {
     'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/comma-dangle': 'off',
+    '@typescript-eslint/indent': 'off',
+    '@typescript-eslint/member-delimiter-style': 'off',
     '@typescript-eslint/no-misused-promises': [
       'error',
       { checksVoidReturn: { attributes: false } },
     ],
+    '@typescript-eslint/space-before-function-paren': 'off',
   },
 }
