6059082311f91c9156b7a9be11949f6d17dff66b
royisy <28505196+royisy@users.noreply.github.com>
2023-05-23 14:17:56 +0900

    fix practice styles

--- src/components/Practice.tsx
+++ src/components/Practice.tsx
@@ -60,71 +60,75 @@ export const Practice = (): JSX.Element => {
   }
 
   return (
-    <>
-      <div className="flex items-center">
-        <button
-          className="btn-ghost btn-square btn w-10 sm:w-12"
-          onClick={() => {
-            navigate('/')
-          }}
-        >
-          <XMarkIcon className="w-10 sm:w-12" />
-        </button>
-        <progress
-          className="progress progress-primary ml-4 h-4 w-full sm:ml-5 sm:h-5"
-          value={index}
-          max={words.length}
-        ></progress>
-      </div>
-      {words.length > 0 && (
-        <>
-          <div className="flex h-28 flex-col items-center sm:h-60">
-            <p className="mt-7 text-3xl sm:mt-24">{words[index]?.definition}</p>
-            <div>
-              {isCorrect && (
-                <CheckIcon className="h-12 w-12 text-green-500 sm:mt-5 sm:h-16 sm:w-16" />
-              )}
-            </div>
-            <p className="mt-1 text-2xl sm:mt-8 sm:text-3xl">{answer}</p>
-          </div>
-          <div className="flex flex-col items-center">
-            <div>
+    <div className="flex justify-center">
+      <div className="w-full max-w-screen-sm p-5">
+        <div className="flex items-center">
+          <button
+            className="btn-ghost btn-square btn w-10 sm:w-12"
+            onClick={() => {
+              navigate('/')
+            }}
+          >
+            <XMarkIcon className="w-10 sm:w-12" />
+          </button>
+          <progress
+            className="progress progress-primary ml-4 h-4 w-full sm:ml-5 sm:h-5"
+            value={index}
+            max={words.length}
+          ></progress>
+        </div>
+        {words.length > 0 && (
+          <>
+            <div className="flex h-28 flex-col items-center sm:h-60">
+              <p className="mt-7 text-3xl sm:mt-24">
+                {words[index]?.definition}
+              </p>
               <div>
-                <input
-                  type="text"
-                  className="input-bordered input-primary input w-72 text-2xl sm:w-96"
-                  ref={inputRef}
-                  value={userAnswer}
-                  onChange={handleAnswerChange}
-                  disabled={answer !== ''}
-                />
+                {isCorrect && (
+                  <CheckIcon className="h-12 w-12 text-green-500 sm:mt-5 sm:h-16 sm:w-16" />
+                )}
               </div>
-              <div className="flex justify-end">
-                {answer === '' && (
-                  <div>
-                    <button
-                      className="btn-outline btn mt-5"
-                      onClick={handleSkipClick}
-                    >
-                      Skip
-                    </button>
-                  </div>
-                )}
-                {answer !== '' && (
-                  <div>
-                    <button
-                      className="btn-primary btn mt-5"
-                      onClick={handleNextClick}
-                    >
-                      Next
-                    </button>
-                  </div>
-                )}
+              <p className="mt-1 text-2xl sm:mt-8 sm:text-3xl">{answer}</p>
+            </div>
+            <div className="flex flex-col items-center">
+              <div>
+                <div>
+                  <input
+                    type="text"
+                    className="input-bordered input-primary input w-72 text-2xl sm:w-96"
+                    ref={inputRef}
+                    value={userAnswer}
+                    onChange={handleAnswerChange}
+                    disabled={answer !== ''}
+                  />
+                </div>
+                <div className="flex justify-end">
+                  {answer === '' && (
+                    <div>
+                      <button
+                        className="btn-outline btn mt-5"
+                        onClick={handleSkipClick}
+                      >
+                        Skip
+                      </button>
+                    </div>
+                  )}
+                  {answer !== '' && (
+                    <div>
+                      <button
+                        className="btn-primary btn mt-5"
+                        onClick={handleNextClick}
+                      >
+                        Next
+                      </button>
+                    </div>
+                  )}
+                </div>
               </div>
             </div>
-          </div>
-        </>
-      )}
-    </>
+          </>
+        )}
+      </div>
+    </div>
   )
 }
