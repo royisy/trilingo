2c3e5a9ba1acc3d0aa0dfe8be255d9c75f9d1508
royisy <28505196+royisy@users.noreply.github.com>
2023-06-06 11:50:37 +0900

    add domain

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/trilingo/' : '',
+  base: '/',
   plugins: [react()],
   build: {
     rollupOptions: {
