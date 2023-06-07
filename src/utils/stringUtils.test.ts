aa62d3557cf97d04a482784c5706cb6ec33dfe39
royisy <28505196+royisy@users.noreply.github.com>
2023-06-07 20:06:57 +0900

    remove spaces and hyphens when checking answer

--- src/utils/stringUtils.test.ts
+++ src/utils/stringUtils.test.ts
@@ -4,9 +4,21 @@ describe('normalizeString', () => {
   it('should remove spaces of beginning and end', () => {
     expect(normalizeString(' abc ')).toEqual('abc')
   })
-  it('should normalize german characters', () => {
-    expect(normalizeString('ä ö ü ß Ä Ö Ü')).toEqual('a o u ss a o u')
+
+  it('should remove spaces and hyphens', () => {
+    expect(normalizeString('a b-c')).toEqual('abc')
   })
+
+  it('should normalize german characters', () => {
+    expect(normalizeString('ä')).toEqual('a')
+    expect(normalizeString('ö')).toEqual('o')
+    expect(normalizeString('ü')).toEqual('u')
+    expect(normalizeString('ß')).toEqual('ss')
+    expect(normalizeString('Ä')).toEqual('a')
+    expect(normalizeString('Ö')).toEqual('o')
+    expect(normalizeString('Ü')).toEqual('u')
+  })
+
   it('should lower case', () => {
     expect(normalizeString('ABC')).toEqual('abc')
   })
--- src/utils/stringUtils.ts
+++ src/utils/stringUtils.ts
@@ -1,10 +1,11 @@
 /**
  * Normalizes a string by performing the following operations:
  * 1. Trimming white spaces from both ends.
- * 2. Replacing diacritics with their base characters
+ * 2. Removing spaces and hyphens.
+ * 3. Replacing diacritics with their base characters
  *    using Unicode Normalization Form D (NFD).
- * 3. Replacing the German sharp s (ß) with 'ss'.
- * 4. Converting the string to lowercase.
+ * 4. Replacing the German sharp s (ß) with 'ss'.
+ * 5. Converting the string to lowercase.
  *
  * @param str The input string to be normalized.
  * @returns The normalized string.
@@ -12,6 +13,7 @@
 export const normalizeString = (str: string): string => {
   return str
     .trim()
+    .replace(/[\s-]/g, '')
     .normalize('NFD')
     .replace(/[\u0300-\u036f]/g, '')
     .replace(/ß/g, 'ss')
